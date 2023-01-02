56cf0643ef828af605a5bb1bc587f36552e8c3bb
Arwen-T <57810506+Arwen-T@users.noreply.github.com>
2023-01-02 20:10:54 +0100

    Changed Euclids_Algorithm
    Changed the older Euclid's algorithm to a more efficient (~15% faster) version of it in every language.

--- Algorithms/Euclids_Algorithm.c
+++ Algorithms/Euclids_Algorithm.c
@@ -1,29 +1,30 @@
 // Euclid's algorithm.
 // Given two positive integers m and n, find their greatest common divisor, that is, the largest positive integer that evenly divides both m and n. 
 
-// 1. [Find remainder.] Divide m by n and let r be the remainder. (We will have 0 <= r < n.)
-// 2. [Is it zero?] If r = 0, the algorithm terminates; n is the answer.
-// 3. [Reduce.] Set m to n, n to r, and go back to step 1.
+// 1. [Remainder m/n.] Divide m by n and let m be the remainder.
+// 2. [Is it zero?] If m = 0, the algorithm terminates; n is the answer.
+// 3. [Remainder n/m.] Divide m by n and let m be the remainder.
+// 4. [Is it zero?] If n = 0, the algorithm terminates; m is the answer; otherwise go back to step 1.
 
 #include <stdbool.h>
 #include <stdio.h>
 
 int getGreatestCommonDivisor(int m, int n) {
-    int r;
-
     while (true) {
-        r = m % n; // 1. [Find remainder.]
-
-        if (r == 0) { // 2. [Is it zero?]
+        m = m % n; // 1. [Remainder m/n.]
+        if (m == 0) { // 2. [Is it zero?]
             return n;
         }
 
-        m = n; // 3. [Reduce.] 
-        n = r;
+        n = n % m; // 3. [Remainder n/m.]
+        if (n == 0) { // 4. [Is it zero?]
+            return m;
+        }
     }
 }
 
 int main() {
-    printf("%d", getGreatestCommonDivisor(423354, 4938));
+    // Test if it works
+    printf("%d", getGreatestCommonDivisor(2166, 6099));
     return 0;
 }
--- Algorithms/Euclids_Algorithm.rs
+++ Algorithms/Euclids_Algorithm.rs
@@ -1,23 +1,26 @@
 // Euclid's algorithm.
 // Given two positive integers m and n, find their greatest common divisor, that is, the largest positive integer that evenly divides both m and n. 
 
-// 1. [Find remainder.] Divide m by n and let r be the remainder. (We will have 0 <= r < n.)
-// 2. [Is it zero?] If r = 0, the algorithm terminates; n is the answer.
-// 3. [Reduce.] Set m to n, n to r, and go back to step 1.
+// 1. [Remainder m/n.] Divide m by n and let m be the remainder.
+// 2. [Is it zero?] If m = 0, the algorithm terminates; n is the answer.
+// 3. [Remainder n/m.] Divide m by n and let m be the remainder.
+// 4. [Is it zero?] If n = 0, the algorithm terminates; m is the answer; otherwise go back to step 1.
 
 fn get_greatest_common_divisor(mut m: u128, mut n: u128) -> u128 {
     loop {
-        let r = m % n;
-
-        if r == 0 {
+        m = m % n; // 1. [Remainder m/n.]
+        if m == 0 { // 2. [Is it zero?]
             return n;
         }
 
-        m = n;
-        n = r;
+        n = n % m; // 3. [Remainder n/m.]
+        if n == 0 { // 4. [Is it zero?]
+            return m;
+        }
     }
 }
 
 fn main() {
-    println!("{}", get_greatest_common_divisor(423354, 4938));
+    // Test if it works
+    println!("{}", get_greatest_common_divisor(2166, 6099));
 }

faf1bd7373f1ad1bc7b916c5218f15f8b269900f
Arwen-T <57810506+Arwen-T@users.noreply.github.com>
2023-01-03 04:11:01 +0100

    Fix Euclids_Algorithm.c
    Fixed issue in explanation of the steps of the algorithm

--- Algorithms/Euclids_Algorithm.c
+++ Algorithms/Euclids_Algorithm.c
@@ -3,7 +3,7 @@
 
 // 1. [Remainder m/n.] Divide m by n and let m be the remainder.
 // 2. [Is it zero?] If m = 0, the algorithm terminates; n is the answer.
-// 3. [Remainder n/m.] Divide m by n and let m be the remainder.
+// 3. [Remainder n/m.] Divide n by m and let n be the remainder.
 // 4. [Is it zero?] If n = 0, the algorithm terminates; m is the answer; otherwise go back to step 1.
 
 #include <stdbool.h>
